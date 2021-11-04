5c39bef9481bef7cdde749f605ba65649f013f8a
Jesús Téllez <jesustellez.contacto@gmail.com>
2021-11-04 20:23:10 +0100

    Linked fields to model
    TODO: just attemp to login

--- src/app/_components/login/login.component.ts
+++ src/app/_components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  user: User = {
+    email: "",
+    password: ""
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
+  loginAttemp() {
+    console.log(this.user);
+  }
 }
--- src/app/_components/register/register.component.ts
+++ src/app/_components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RegisterComponent implements OnInit {
-
+  user: User = {
+    email: "",
+    username: "",
+    password: ""
+  }
   constructor() { }
 
   ngOnInit() {
