c60f542d8100be19a4ddc6604e219191cb3ca6e1
Jesús Téllez <jesustellez.contacto@gmail.com>
2021-11-08 22:06:56 +0100

    Angular theming + login validation

--- src/app/_components/login/login.component.ts
+++ src/app/_components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/_models/user';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,16 +7,21 @@ import { User } from 'src/app/_models/user';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  user: User = {
-    email: "",
-    password: ""
-  }
+  login!: FormGroup;
+  hidePassword = true;
   constructor() { }
 
   ngOnInit() {
+    this.login = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
   }
 
-  loginAttemp() {
-    console.log(this.user);
+  onLoginPressed() {
+    const email = this.login.get('email')?.value;
+    const password = this.login.get('password')?.value;
+    console.log(email + ' ' + password);
   }
+
 }
